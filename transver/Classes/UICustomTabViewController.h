ff3afe4d48e1d19ab3c49534ee665c8463367511
poioitslash33@gmail.com <poioitslash33@gmail.com@781a8ce1-9a28-9206-2ec8-73ebfc126fc9>
2011-08-16 00:31:59 +0000

    add the download function

--- transver/Classes/UICustomTabViewController.h
+++ transver/Classes/UICustomTabViewController.h
@@ -15,9 +15,15 @@
 	IBOutlet UITabBarItem *moreTabBarItem;
 	IBOutlet UITabBarItem *recordingTabBarItem;
 	UIViewController *selectedViewController;
+    NSString* localFilePath;
+    NSFileHandle* audioFile;
 }
 
+- (void) downloadToFile;
+
 @property (nonatomic, retain) NSArray *viewControllers;
+@property (nonatomic, retain) NSString *localFilePath;
+@property (nonatomic, retain) NSFileHandle *audioFile;
 @property (nonatomic, retain) IBOutlet UITabBar *tabBar;
 @property (nonatomic, retain) IBOutlet UITabBarItem *favouritesTabBarItem;
 @property (nonatomic, retain) IBOutlet UITabBarItem *moreTabBarItem;
