a9d7e220616a7a6e01a8a506d1f99e39c3e670b5
poioitslash33@gmail.com <poioitslash33@gmail.com@781a8ce1-9a28-9206-2ec8-73ebfc126fc9>
2011-08-21 12:10:17 +0000

    commit after xcode4 upgrade

---
+++ trunk/transver/Classes/ChatBubbleView.h
@@ -0,0 +1,32 @@
+//
+//  ChatBubbleView.h
+//  ChatMe
+//
+//  Created by Emerson Malca on 6/24/11.
+//  Copyright 2011 OneZeroWare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol ChatBubbleViewDelegate;
+
+@interface ChatBubbleView : UIView <UITextViewDelegate> {
+    BOOL initialSetupDone;
+}
+
+@property (nonatomic, retain) IBOutlet UIImageView *bgImageView;
+@property (nonatomic, retain) IBOutlet UITextView *messageTextView;
+@property (nonatomic, retain) IBOutlet UIButton *sendBtn;
+@property (nonatomic, assign) id<ChatBubbleViewDelegate> delegate;
+
+- (IBAction)sendButtonTapped;
+
+@end
+
+@protocol ChatBubbleViewDelegate <NSObject>
+@optional
+
+- (void)chatBubbleView:(ChatBubbleView *)bubbleView willResizeToHeight:(CGFloat)newHeight;
+- (void)chatBubbleView:(ChatBubbleView *)bubbleView willSendText:(NSString *)message;
+
+@end
