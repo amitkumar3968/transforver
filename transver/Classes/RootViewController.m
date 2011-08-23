232bfd24a6f374eff8ed7a2efc23c64a7486380b
brendonfish <brendonfish@781a8ce1-9a28-9206-2ec8-73ebfc126fc9>
2011-08-23 18:13:00 +0000

    Implement playSound method

--- transver/Classes/RootViewController.m
+++ transver/Classes/RootViewController.m
@@ -119,14 +119,17 @@
 	[audioRecorder startPlayback];*/
 	system("ls");
 	SystemSoundID soundID = 0;
+<<<<<<< .mine
+    NSString *filePath = [NSString stringWithFormat:@"%@/%@.aif", [Util getDocumentPath], @"out"];
+=======
 	//NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
 	NSString *documentsPath = [Util getDocumentPath];
 	NSString *audioPath = [documentsPath stringByAppendingPathComponent:@"out.aif"];
     NSString *filePath = [NSString stringWithFormat:@"%@/%@.aif", [Util getDocumentPath], @"recording"];
+>>>>>>> .r49
 	//NSString *filePath = [[NSBundle mainBundle] resourcePath];// stringByAppendingPathComponent:@"123.wav"];
-	NSLog(documentsPath);
 	NSLog(filePath);
-	NSURL* tmpUrl = [[NSURL alloc] initFileURLWithPath:audioPath ];
+	NSURL* tmpUrl = [[NSURL alloc] initFileURLWithPath:filePath ];
 	CFURLRef soundFileURL = (CFURLRef)tmpUrl;
 	OSStatus errorCode = AudioServicesCreateSystemSoundID(soundFileURL, &soundID);
 	if (errorCode != 0) {
