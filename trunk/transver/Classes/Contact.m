e2961d37f6a55c711b654f408e0636a4055a0f13
poioitslash33@gmail.com <poioitslash33@gmail.com@781a8ce1-9a28-9206-2ec8-73ebfc126fc9>
2011-09-10 01:06:38 +0000

    add some chat user interface

---
+++ trunk/transver/Classes/Contact.m
@@ -0,0 +1,24 @@
+//
+//  Contact.m
+//  ChatMe
+//
+//  Created by Emerson Malca on 6/24/11.
+//  Copyright (c) 2011 OneZeroWare. All rights reserved.
+//
+
+#import "Contact.h"
+#import "ChatMeUser.h"
+
+
+@implementation Contact
+@dynamic isBlocked;
+@dynamic isChatOpen;
+//If the contact user blocked you, the visible contact user status is offline
+@dynamic visibleContactUserStatus;
+@dynamic user;
+@dynamic contactUser;
+@dynamic lastCommunicationText;
+
+
+
+@end
