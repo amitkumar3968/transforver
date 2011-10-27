87d107585a925dc5bde17f52b746f9b01085282b
poioitslash33@gmail.com <poioitslash33@gmail.com@781a8ce1-9a28-9206-2ec8-73ebfc126fc9>
2011-10-27 03:15:33 +0000

    show the message in dialog

--- transver/Classes/ChatViewController.h
+++ transver/Classes/ChatViewController.h
@@ -13,6 +13,7 @@
 #import "JSON.h"
 #import "SectionInfo.h"
 #import "SectionHeaderView.h"
+#import "MessageTableViewCell.h"
 
 @class Contact;
 
@@ -21,6 +22,7 @@
     NSTimer *myTimer;
     NSInteger openSectionIndex;
     NSMutableArray* sectionInfoArray;
+    NSMutableArray *m_Messages;
 }
 
 @property (nonatomic, retain) NSMutableArray* sectionInfoArray;
@@ -30,8 +32,11 @@
 @property (nonatomic, retain) Contact *contact;
 @property (nonatomic, retain) NSMutableArray *listOfItems;
 @property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, retain) NSMutableArray *m_Messages;
+
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView;
 - (NSArray*) fetchMessages:(int) uid DstID:(int)dstid;
 - (NSArray*) sendMessages:(int) uid;
 - (id) initWithRelation: (int) srcid DstID:(int) dstid;
+- (void)configureCell:(MessageTableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath ;
 @end
