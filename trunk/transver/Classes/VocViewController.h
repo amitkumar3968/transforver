cdf490d9a8f2155082e80ee27aac1aaf849e1934
brendonfish <brendonfish@781a8ce1-9a28-9206-2ec8-73ebfc126fc9>
2011-11-05 05:21:13 +0000

    call revised vocode view controller

--- trunk/transver/Classes/VocViewController.h
+++ trunk/transver/Classes/VocViewController.h
@@ -10,7 +10,23 @@
 
 
 @interface VocViewController : UIViewController {
-
+	IBOutlet UITextField *passwd;
+	IBOutlet UIButton *dovocode;
+	IBOutlet UIButton *playorig;
+	IBOutlet UIButton *playtrans;
+	IBOutlet UIButton *record;
+	IBOutlet UIButton *sendexit;
+	IBOutlet UIButton *quit;
+	IBOutlet UIPickerView *voice_opt;
+	IBOutlet UISwitch *encrypt;
 }
+
+- (IBAction) playorig_playback;
+- (IBAction) playtrans_playback;
+- (IBAction) dovocode_playback;
+- (IBAction) record_playback;
+- (IBAction) sendexit_playback;
+- (IBAction) quit_playback;
+
 
 @end
