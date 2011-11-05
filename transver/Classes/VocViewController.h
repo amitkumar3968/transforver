40544309fc067d62755f4435668f2c27373ba272
brendonfish <brendonfish@781a8ce1-9a28-9206-2ec8-73ebfc126fc9>
2011-11-05 05:08:42 +0000

    revised vocode view controller

---
+++ transver/Classes/VocViewController.h
@@ -0,0 +1,16 @@
+//
+//  VocViewController.h
+//  NavTab
+//
+//  Created by easystudio on 11/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface VocViewController : UIViewController {
+
+}
+
+@end
