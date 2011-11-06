00d6f3803ecf3e4f585b485e7705e1fb40ee41b3
brendonfish <brendonfish@781a8ce1-9a28-9206-2ec8-73ebfc126fc9>
2011-11-06 16:55:41 +0000

    realize vocodeTapped function

--- transver/Classes/VocViewController.m
+++ transver/Classes/VocViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "VocViewController.h"
+#include "dovocode.h"
 
 
 @implementation VocViewController
+
+@synthesize audioFile;
+@synthesize audioRecorder;
+@synthesize localFilePath;
 
 // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
 
@@ -35,8 +40,6 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
-@synthesize audioFile;
-@synthesize audioRecorder;
 
 - (void)didReceiveMemoryWarning {
     // Releases the view if it doesn't have a superview.
@@ -54,7 +57,7 @@
 - (void)initialSetup {
     UIImage *img = [UIImage imageNamed:@"bgTextBubbleLC20TC20"];
     UIImage *bg = [img stretchableImageWithLeftCapWidth:20 topCapHeight:20];
-    //audioRecorder = [[[AudioRecorder	alloc] init] retain];	
+    audioRecorder = [[[AudioRecorder	alloc] init] retain];	
 }
 
 
@@ -62,20 +65,78 @@
     [super dealloc];
 }
 
+- (void) uploadFile:(char *)filepath {
+	/*
+	 turning the image into a NSData object
+	 getting the image back out of the UIImageView
+	 setting the quality to 90
+	 */
+	UIImageView *image = [[UIImageView alloc] initWithFrame:
+                          CGRectMake(100.0, 100.0, 57.0, 57.0)];
+	image.image = 
+	[UIImage imageNamed:@"phone.png"];
+	//NSData *imageData = UIImageJPEGRepresentation(image.image, 90);
+	//NSString* str =  [[NSBundle mainBundle] pathForResource:@"mysoundcompressed" ofType:@"caf"];
+	NSString *audioFile = [NSString stringWithFormat:@"%@/%@.aif", [Util getDocumentPath], @"recording"]; 
+	NSData *wavData = [NSData dataWithContentsOfFile:audioFile];
+	// setting up the URL to post to
+	NSString *urlString = @"http://www.entalkie.url.tw/upload.php";
+	
+	// setting up the request object now
+	NSMutableURLRequest *request = [[[NSMutableURLRequest alloc] init] autorelease];
+	[request setURL:[NSURL URLWithString:urlString]];
+	[request setHTTPMethod:@"POST"];
+	
+	/*
+	 add some header info now
+	 we always need a boundary when we post a file
+	 also we need to set the content type
+	 
+	 You might want to generate a random boundary.. this is just the same
+	 as my output from wireshark on a valid html post
+	 */
+	NSString *boundary = [NSString stringWithString:@"---------------------------14737809831466499882746641449"];
+	NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",boundary];
+	[request addValue:contentType forHTTPHeaderField: @"Content-Type"];
+	
+	/*
+	 now lets create the body of the post
+	 */
+	NSMutableData *body = [NSMutableData data];
+	[body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+	[body appendData:[[NSString stringWithString:@"Content-Disposition: form-data; name=\"userfile\"; filename=\"star.caf\"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+	[body appendData:[[NSString stringWithString:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+	[body appendData:[NSData dataWithData:wavData]];
+	[body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+	// setting the body of the post to the reqeust
+	[request setHTTPBody:body];
+	
+	// now lets make the connection to the web
+	NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+	NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+	
+	NSLog(@"%@",returnString);
+}
+
 - (IBAction)vocodeTapped :(id)sender{
 	NSLog(@"show up!");
+	NSLog(@"stop recording!");
+	NSLog(@"playing...");
+	[Util copyFile];
+	NSString *audioFile = [NSString stringWithFormat:@"%@/%@.aif", [Util getDocumentPath], @"recording"];
+	const char *modulator_filepath = [audioFile UTF8String];
+	audioFile = [NSString stringWithFormat:@"%@/%@.aif", [Util getDocumentPath], @"Lion"];
+	const char *carrier_filepath = [audioFile UTF8String];
+	audioFile = [NSString stringWithFormat:@"%@/%@.aif", [Util getDocumentPath], @"out"];
+	const char *output_filepath=[audioFile UTF8String];
+	audioFile = [NSString stringWithFormat:@"%@/%@.aif", [Util getDocumentPath], @"meta"];
+	const char *meta_filepath=[audioFile UTF8String];								
+	int encrypt=1;
+	dovocode(encrypt, output_filepath, meta_filepath, modulator_filepath, carrier_filepath);
+	[self uploadFile:modulator_filepath];
+	///[self uploadFile:recording_filepath];
 }
 
-- (void)recordButtonTapped {
-    NSLog(@"Record");
-	[audioRecorder startRecording];
-}
-
-- (void)recordButtonTouchUp {
-	[audioRecorder stopRecording];
-    NSLog(@"Record stop");
-
-}
 
 - (IBAction) playorig_playback{
     NSLog(@"Record stop");
