d4689ee9d82edcba46ee30ae8d64c2080e75a652
brendonfish <brendonfish@781a8ce1-9a28-9206-2ec8-73ebfc126fc9>
2011-11-07 17:46:16 +0000

    add UIDataPickerView related procedures vocode with different carriers based on picker selection

--- transver/Classes/VocViewController.h
+++ transver/Classes/VocViewController.h
@@ -10,13 +10,16 @@
 #import "AudioRecorder.h"
 #import "Util.h"
 
-@interface VocViewController : UIViewController {
+@interface VocViewController : UIViewController 
+<UIPickerViewDelegate, UIPickerViewDataSource>{
 	IBOutlet UITextField *password;
 	IBOutlet UIPickerView *voice_opt;
 	IBOutlet UISwitch *encrypt;
+	NSArray *vocodeCarrierOptions;
 	NSString* localFilePath;
 	AudioRecorder *audioRecorder;
 	int done_vocode;
+	int vocode_carrier_index;
 }
 
 @property (nonatomic,retain)  AudioRecorder *audioRecorder;
@@ -25,6 +28,7 @@
 @property (nonatomic, retain) UITextField *password;
 @property (nonatomic, retain) UIPickerView *voice_opt;
 @property (nonatomic, retain) UISwitch *encrypt;
+@property (nonatomic, retain) NSArray *vocodeCarrierOptions;
 
 - (IBAction) playorig_playback:(id)sender;
 - (IBAction) playtrans_playback;
@@ -32,7 +36,6 @@
 - (IBAction) quit_playback:(id)sender;
 - (IBAction) vocodeTapped :(id)sender;
 - (void) uploadFile:(char*)filepath;
-
 
 
 @end
