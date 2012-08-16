9188f415d0cee4d2d06898d587da52c60cf18621
poioitslash33@gmail.com <poioitslash33@gmail.com@781a8ce1-9a28-9206-2ec8-73ebfc126fc9>
2012-08-16 15:22:13 +0000

    add history controller

---
+++ trunk/transver/vwHistoryController.h
@@ -0,0 +1,13 @@
+//
+//  vwHistoryController.h
+//  NavTab
+//
+//  Created by hank chen on 8/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface vwHistoryController : UIViewController <UINavigationBarDelegate>
+
+@end
